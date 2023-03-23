4bb2a4dc6caec26ebac6c0014123f877977b874e
HamzaNorov <bahtiernorov@gmail.com>
2023-03-23 20:48:18 +0300

    визитка

---
+++ work1.0/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("************************");
+        System.out.println("****     Визитка    ****");
+        System.out.println("****Name:\"you name\" ****");
+        System.out.println("****Skype: you skype****");
+        System.out.println("************************");
+
+    }
+}
